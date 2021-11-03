3135e1b9d5b2881fee41580fcf904837c2742736
Himmel König <itogreniyorum@gmail.com>
2021-11-03 21:52:11 +0300

    admin bug fixed

--- home/admin.py
+++ home/admin.py
@@ -27,8 +27,8 @@ class TeacherAdmin(admin.ModelAdmin):
 		'updatedDate',
 		'createdDate',
 	)
-  list_filter = ('firs_name',)
-  search_fields = ('name__startswith',)
+  list_filter = ('first_name',)
+  search_fields = ('first_name__startswith',)
 
 admin.site.register(Teacher, TeacherAdmin)
 admin.site.register(Branch)
